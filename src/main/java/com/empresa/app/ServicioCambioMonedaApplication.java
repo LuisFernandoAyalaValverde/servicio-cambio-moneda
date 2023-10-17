37d4836dfd74c0642ca402725d58dfa21c2a7fbe
Luis Ayala <reizer_2015@hotmail.com>
2023-10-17 09:22:15 -0500

    inicio

---
+++ src/main/java/com/empresa/app/ServicioCambioMonedaApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCambioMonedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCambioMonedaApplication.class, args);
+	}
+
+}
